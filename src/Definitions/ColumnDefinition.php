a00cf5e7942ece7796b76c2fcdec71c19f2e623a
Chris Thompson <chris@marketmuscles.com>
2021-11-24 10:39:41 -0500

    This shouldn't have been removed

--- src/Definitions/ColumnDefinition.php
+++ src/Definitions/ColumnDefinition.php
@@ -4,6 +4,7 @@ namespace LaravelMigrationGenerator\Definitions;
 
 use Illuminate\Support\Str;
 use LaravelMigrationGenerator\Helpers\ValueToString;
+use LaravelMigrationGenerator\Helpers\WritableTrait;
 
 /**
  * Class ColumnDefinition
