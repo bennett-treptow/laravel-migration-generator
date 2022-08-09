ba257c854f37e096ca6b56a26e1323b26fedccec
Bennett Treptow <bennett-treptow@users.noreply.github.com>
2022-08-08 20:08:12 -0500

    Use Database Prefix (#71)
    * Use database prefix (if applicable)
    
    * cs fix
    
    * Add test for not having a prefix specified

--- tests/Unit/GeneratorManagers/MySQLGeneratorManagerTest.php
+++ tests/Unit/GeneratorManagers/MySQLGeneratorManagerTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\GeneratorManagers;
 
 use Tests\TestCase;
 use Mockery\MockInterface;
+use Illuminate\Support\Facades\DB;
 use LaravelMigrationGenerator\Definitions\IndexDefinition;
 use LaravelMigrationGenerator\Definitions\TableDefinition;
 use LaravelMigrationGenerator\Definitions\ColumnDefinition;
@@ -51,5 +52,34 @@ class MySQLGeneratorManagerTest extends TestCase
         $sorted = $mocked->sortTables($mocked->getTableDefinitions());
         $this->assertCount(4, $sorted);
         $this->assertStringContainsString('$table->dropForeign', $sorted[3]->formatter()->stubTableDown());
+    }
+
+    public function test_can_remove_database_prefix()
+    {
+        $connection = DB::getDefaultConnection();
+        config()->set('database.connections.' . $connection . '.prefix', 'wp_');
+
+        $mocked = $this->partialMock(MySQLGeneratorManager::class, function (MockInterface $mock) {
+            $mock->shouldReceive('init');
+        });
+
+        $definition = (new TableDefinition())->setTableName('wp_posts');
+        $mocked->addTableDefinition($definition);
+        $this->assertEquals('posts', $definition->getTableName());
+
+        $definition = (new TableDefinition())->setTableName('posts');
+        $mocked->addTableDefinition($definition);
+        $this->assertEquals('posts', $definition->getTableName());
+
+
+        config()->set('database.connections.' . $connection . '.prefix', '');
+
+        $definition = (new TableDefinition())->setTableName('wp_posts');
+        $mocked->addTableDefinition($definition);
+        $this->assertEquals('wp_posts', $definition->getTableName());
+
+        $definition = (new TableDefinition())->setTableName('posts');
+        $mocked->addTableDefinition($definition);
+        $this->assertEquals('posts', $definition->getTableName());
     }
 }
